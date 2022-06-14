9f7f8ac0d273c9ca800a6578d0c192df288f6d3a
201870024 <201870024@smail.nju.edu.cn>
2022-06-14 23:33:06 +0800

    完成了评论页面的框架，还没有测试

--- miniprogram/pages/me/comment.js
+++ miniprogram/pages/me/comment.js
@@ -5,14 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userID: 0,
+    book_comments: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    //获取userid
+    var userID = wx.getStorageSync('openid');
+    console.log(userID);
+    this.setData({
+      userID: userID,
+    });
 
+    //通过userid获取评论信息
+    wx.cloud
+      .callContainer({
+        config: {
+          env: "prod-8gt4mz04386985ef",
+        },
+        path: "/api/commentGet",
+        header: {
+          "X-WX-SERVICE": "golang-6i3q",
+        },
+        method: "POST",
+        data: {
+          action: "user",
+          hint: ""+userID,
+        },
+      })
+      .then((resp) => {
+        var t = resp.data.data;
+        console.log(t);
+        this.setData({
+          number_of_book_comments: t.length,
+          book_comments: t
+        });
+      })
+      .catch((e) => {
+      });
   },
 
   /**
